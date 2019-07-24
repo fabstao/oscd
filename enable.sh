423b581369d6abbb1f7235defb352b3fbfce2dff
fsalam1 <fabian.salamanca@intel.com>
2019-07-24 14:59:00 -0500

    Enable OSCD service

---
+++ enable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+WANTS=/etc/systemd/system/multi-user.target.wants
+if [ ! -d ${WANTS} ]; then
+	mkdir -p ${WANTS}
+fi
+ cd /etc/systemd/system/multi-user.target.wants && ln -s /usr/lib/systemd/system/oscd.service 
